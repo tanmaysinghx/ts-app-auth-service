fdbb4abcacf48c9f3f7222af086c5746d1799edf
Tanmay Singh <tanmaysinghx@gmail.com>
2024-01-14 23:13:09 +0530

    Added logger

--- src/main/java/com/auth/AuthApplication.java
+++ src/main/java/com/auth/AuthApplication.java
@@ -10,7 +10,7 @@ public class AuthApplication {
 
 
     public static void main(String[] args) {
-    	
+    	System.out.println("tes-app-auth-service is running");
         SpringApplication.run(AuthApplication.class, args);
 
     }
